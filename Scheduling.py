6336b93a3a1c20021df3467962df95711f3e0d2b
tylereriksen <saeji.hong@gmail.com>
2022-04-12 18:50:48 -0700

    Update

--- Scheduling.py
+++ Scheduling.py
@@ -1,26 +1,73 @@
 import pandas as pd
 import numpy as np
 
-def roundUpNearestHalf(num):
-    listHalf = list(np.arange(0,24, 0.5))
-    intReturn = [x for x in listHalf if 0 <= x - num < 0.5]
+BUFFER = 0.5
+TIMEDURATION = 1
+INTERVAL = 0.25
+ERROR_MESSAGE = "ERROR"
+
+def roundUpNearestInterval(num):
+    listHalf = list(np.arange(0,24, INTERVAL))
+    intReturn = [x for x in listHalf if 0 <= x - num < INTERVAL]
     return intReturn[0]
 
-def roundDownNearestHalf(num):
-    listHalf = list(np.arange(0,24, 0.5))
-    intReturn = [x for x in listHalf if 0 <= num - x < 0.5]
+def roundDownNearestInterval(num):
+    listHalf = list(np.arange(0,24, INTERVAL))
+    intReturn = [x for x in listHalf if 0 <= num - x < INTERVAL]
     return  intReturn[0]
 
+def timeToNumber(stringTime):
 
+    hrminList = stringTime.split(":")
+    if not len(hrminList) == 2:
+        return (ERROR_MESSAGE)
+
+    try:
+        hours = float(hrminList[0])
+        min = float(hrminList[1])/60
+    
+    except:
+        return (ERROR_MESSAGE)
+
+    return (hours + min)
+
+def numberToTime(numTime):
+
+    # check if input is valid
+    if type(numTime) == type("") or numTime < 0 or numTime >= 24:
+        return ERROR_MESSAGE
+
+    hours = int(numTime)
+    min = str(int((numTime - hours) * 60))
+
+    # case for when minutes is 0, or else it would be printed as ":0" instead of ":00"
+    if min == "0":
+        min = "00"
+
+    return str(hours) + ":" + min
 
 
 employeeWorkTimes = {
     'Employee Name': ['John Joe', 'Henry McGuire', 'Tony Toni'],
-    'Shift Start Time': ['7:30', '10:30', '13:30'],
-    'Shift End Time': ['2:00', '18:30', '21:00']
+    'Shift Start Time': ['7:00', '10:30', '13:30'],
+    'Shift End Time': ['14:00', '18:30', '21:00']
 }
 df = pd.DataFrame(data=employeeWorkTimes)
 
 employeeWorkTimesDict = {}
 for idx, num in enumerate(df['Employee Name']):
     employeeWorkTimesDict[num] = [df['Shift Start Time'][idx], df['Shift End Time'][idx]]
+
+def convertDictToList(companyDict, timeDuration = TIMEDURATION):
+    returnFixedDict = {}
+    for idx, name in enumerate(companyDict.keys()):
+        workingDef = companyDict[name]
+        startTime = roundUpNearestInterval(timeToNumber(workingDef[0]))
+        endTime = roundDownNearestInterval(timeToNumber(workingDef[1]))
+        listOfTimes = list(np.arange(startTime, endTime - TIMEDURATION + INTERVAL, INTERVAL))
+        returnFixedDict[name] = listOfTimes
+
+    return returnFixedDict
+
+
+print(convertDictToList(employeeWorkTimesDict))
