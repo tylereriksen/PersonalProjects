bcff8ca914782acb88b2a59ec48cd18cad3ebbac
tylereriksen <saeji.hong@gmail.com>
2022-05-07 20:37:49 -0700

    Update SearchGraph.py

--- SearchGraph.py
+++ SearchGraph.py
@@ -1,12 +1,19 @@
 graph = {
-    0: [1, 2, 3],
-    1: [0, 3],
-    2: [0, 3],
-    3: [0, 1, 2, 4],
-    4: [3]
+    0: [1, 2],
+    1: [0, 2, 3, 4],
+    2: [0, 1],
+    3: [1, 5],
+    4: [1],
+    5: [3, 6, 7, 8],
+    6: [5],
+    7: [5, 8],
+    8: [5, 7, 9],
+    9: [8]
 }
 
 def BFS(G, start):
+
+    # make sure the starting Node is in the graph
     if start not in set(G.keys()):
         raise Exception("Start node is invalid")
 
@@ -27,5 +34,26 @@ def isBFSComplete(G, start):
         return True
     return False
 
-print(BFS(graph, 1))
-# print(isBFSComplete(graph, 0))
+def DFS(G, start):
+
+    # make sure the starting Node is in the graph
+    if start not in set(G.keys()):
+        raise Exception("Start node is invalid")
+
+    visited = []
+
+    # inner function to search the graph
+    def DFS_algo(G, start):
+        stack = [start]
+        while len(stack) > 0:
+            node = stack.pop()
+            if node not in set(visited):
+                visited.append(node)
+                for neighbors in set(G[node]):
+                    if neighbors not in set(visited):
+                        stack.append(neighbors)
+    DFS_algo(G, start)
+    return visited
+
+print(BFS(graph, 0))
+print(DFS(graph, 0))
