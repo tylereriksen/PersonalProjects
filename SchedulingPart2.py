9b3d9b4e2fbb4bc993f5b6d94b7619e1569eacc0
tylereriksen <saeji.hong@gmail.com>
2022-05-14 16:07:56 -0700

    Create SchedulingPart2.py

---
+++ SchedulingPart2.py
@@ -0,0 +1,4 @@
+'''
+    This file will attempt to download the csv file that contains all of the customer's appointment requests
+    and tries to assign workers based on their availability and work notice.
+'''
